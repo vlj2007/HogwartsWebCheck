7ba7ab3696711928d18d23199fe8bf4f9832ceb2
vlj2007 <vlj2007@yandex.ru>
2024-01-31 15:29:17 +0700

    init commit

---
+++ src/main/java/ru/hogwarts/hogwartswebcheck/HogwartsWebCheckApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.hogwartswebcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsWebCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogwartsWebCheckApplication.class, args);
+	}
+
+}
